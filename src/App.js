369fadc1b805024bf0ec1d2ab5c8c36c63c2b85c
Claire Lu <appsfunlu@gmail.com>
2022-01-13 18:45:04 -0800

    update

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>CoreScore</h1>
+        <h4>Privacy Policy</h4>
+        <p>
+          This privacy policy will help you understand how CoreScore uses and
+          protects the data you provide to us when you visit and use this app.
+        </p>
+        <p>
+          We reserve the right to change this policy at any given time, of which
+          you will be promptly updated. If you want to make sure that you are up
+          to date with the latest changes, we advise you to frequently visit
+          this page.
+        </p>
+        <h5>What User Data We Collect</h5>
+        <p>
+          When you visit the website, we may collect the following
+          data:Name,Email,gender,age,fitness score
+        </p>
+        <h5>We are collecting your data for several reasons:</h5>
+        <p>
+          To improve our services and products,To send you promotional emails
+          containing the information we think you will find interesting.
+        </p>
+        <h5>Safeguarding and Securing the Data</h5>
+        <p>
+          Corescore is committed to securing your data and keeping it
+          confidential. CoreScore has done all in its power to prevent data
+          theft, unauthorized access, and disclosure by implementing the latest
+          technologies and software, which help us safeguard all the information
+          we collect online.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
